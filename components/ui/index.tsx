0bbe011866537ef4d910f46b718a0069fb5857a9
thin.tranhoang <thin.tranhoang@ncc.asia>
2025-12-10 15:48:35 +0700

    update add

---
+++ components/ui/index.tsx
@@ -0,0 +1,6 @@
+export { Button } from './button';
+export { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from './card';
+export { Input } from './input';
+export { Textarea } from './textarea';
+export { Avatar, AvatarImage, AvatarFallback } from './avatar';
+export { Badge } from './badge';
